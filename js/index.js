545d29d4767e2d8081c459242a8ef638a7643181
sy0601 <s_yan0601@163.com>
2017-02-04 16:35:28 +0800

    update 2.4

---
+++ js/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by hongliang.zhao on 2017/1/29.
+ */
+//music
+(function(){
+    $(function(){
+        var oAudio = $('<audio></audio>');
+        oAudio.attr('src','music/Jam - 七月上.mp3');
+        oAudio[0].play();
+    })
+})();
